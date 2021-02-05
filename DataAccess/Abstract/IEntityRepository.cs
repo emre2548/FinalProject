9efbcbb0556087aedc3edea904e4fac207a15f60
Emre Demir <emre2548@gmail.com>
2021-02-05 19:43:47 +0300

    FirstCommit

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    // generic constrain -- generik kısıt demek buraya gelecek değişken tiplerini sınırlandırıyoruz
+    // class : referans tip olabilir demek
+    // IEnttiy : I enttiy olabilir veya IEntity implemente edebilen bir nesne olabilir
+    // new() ; new'lenebilir olmalı
+    // T -- IEntity olabilir ya da IEntity'den implemente edilen bir nesne olabilir demek
+    // IEntity'yi new ile devre dışı bıraktık çünkü IEntity new'lenemez  new ile newlenebilir olmalı dedik böylece IEntity gelemez
+
+    /* Ayakları üzerinde durabilen bir backend yazmış olduk */
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         /* buradaki yapıyı ICategoryDal ve IProductDal içinde kullanıyoruz aynısını sadece tipi değişiyor 
          * her iki interface de ayrı ayrı yazmaya gerek kalmadan buradan T ile gelen tipi alıp o tipte işlem 
