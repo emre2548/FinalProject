fbb3be80c02ed59af5745e7e40909f74415844bd
Emre Demir <emre2548@gmail.com>
2021-03-21 13:52:12 +0300

    20 Mart Commit 2

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
@@ -35,9 +36,22 @@ namespace Core.Extensions
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "Internal Server Error";
+            IEnumerable<ValidationFailure> errors;
             if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
+                errors = ((ValidationException)e).Errors;
+                httpContext.Response.StatusCode = 400;
+
+                // validationa uygun olan bir hata nesnesi oluşturduk
+                // doğrulama hatası ise ona göre bir dönüş gerçekleştirdik
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
+                {
+                    StatusCode = 400,
+                    Message = message,
+                    Errors = errors
+                }.ToString());
+
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
